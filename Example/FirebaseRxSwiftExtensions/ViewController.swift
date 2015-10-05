8722df975fc8d6754003e4db2caaaf847d9f7c58
Maximilian Alexander <max@zinkpulse.com>
2015-10-04 20:06:59 -0700

    Initial commit

---
+++ Example/FirebaseRxSwiftExtensions/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FirebaseRxSwiftExtensions
+//
+//  Created by Maximilian Alexander on 10/04/2015.
+//  Copyright (c) 2015 Maximilian Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
